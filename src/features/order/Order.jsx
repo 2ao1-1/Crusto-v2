411bd93b39cff904bfb5c8b02d427bf4b8671624
Your GitHub Username <Your GitHub Email>
2025-04-08 13:49:19 +0200

    update styles and improve layout across components

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,110 +1,111 @@
+import OrderItem from '../order/OrderItem';
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
-import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from 'react-router-dom';
+import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utils/helpers";
+} from '../../utils/helpers';
 
 // const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
+//   id: 'ABCDEF',
+//   customer: 'Jonas',
+//   phone: '123456789',
+//   address: 'Arroios, Lisbon , Portugal',
 //   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
+//   estimatedDelivery: '2027-04-25T10:00:00',
 //   cart: [
 //     {
 //       pizzaId: 7,
-//       name: "Napoli",
+//       name: 'Napoli',
 //       quantity: 3,
 //       unitPrice: 16,
 //       totalPrice: 48,
 //     },
 //     {
 //       pizzaId: 5,
-//       name: "Diavola",
+//       name: 'Diavola',
 //       quantity: 2,
 //       unitPrice: 16,
 //       totalPrice: 32,
 //     },
 //     {
 //       pizzaId: 3,
-//       name: "Romana",
+//       name: 'Romana',
 //       quantity: 1,
 //       unitPrice: 15,
 //       totalPrice: 15,
 //     },
 //   ],
-//   position: "-9.000,38.000",
+//   position: '-9.000,38.000',
 //   orderPrice: 95,
 //   priorityPrice: 19,
 // };
 
-// const order = {
-//   customer: "Jonas Schmedtmann",
-//   status: "delivered",
-//   priority: true,
-//   cart: [
-//     {
-//       addIngredients: [],
-//       removeIngredients: [],
-//       pizzaId: 1,
-//       name: "Margherita",
-//       quantity: 2,
-//       unitPrice: 12,
-//       totalPrice: 24,
-//     },
-//     {
-//       addIngredients: [],
-//       removeIngredients: [],
-//       pizzaId: 4,
-//       name: "Prosciutto e Rucola",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       addIngredients: [],
-//       removeIngredients: [],
-//       pizzaId: 6,
-//       name: "Vegetale",
-//       quantity: 1,
-//       unitPrice: 13,
-//       totalPrice: 13,
-//     },
-//     {
-//       addIngredients: [],
-//       removeIngredients: [],
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       addIngredients: [],
-//       removeIngredients: [],
-//       pizzaId: 11,
-//       name: "Spinach and Mushroom",
-//       quantity: 2,
-//       unitPrice: 15,
-//       totalPrice: 30,
-//     },
-//   ],
-//   id: "IIDSAT",
-//   estimatedDelivery: "2027-04-25T06:42:22",
-//   // estimatedDelivery: "2027-04-25T10:00:00",
+const order = {
+  customer: 'Jonas Schmedtmann',
+  status: 'delivered',
+  priority: true,
+  cart: [
+    {
+      addIngredients: [],
+      removeIngredients: [],
+      pizzaId: 1,
+      name: 'Margherita',
+      quantity: 2,
+      unitPrice: 12,
+      totalPrice: 24,
+    },
+    {
+      addIngredients: [],
+      removeIngredients: [],
+      pizzaId: 4,
+      name: 'Prosciutto e Rucola',
+      quantity: 3,
+      unitPrice: 16,
+      totalPrice: 48,
+    },
+    {
+      addIngredients: [],
+      removeIngredients: [],
+      pizzaId: 6,
+      name: 'Vegetale',
+      quantity: 1,
+      unitPrice: 13,
+      totalPrice: 13,
+    },
+    {
+      addIngredients: [],
+      removeIngredients: [],
+      pizzaId: 7,
+      name: 'Napoli',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+    {
+      addIngredients: [],
+      removeIngredients: [],
+      pizzaId: 11,
+      name: 'Spinach and Mushroom',
+      quantity: 2,
+      unitPrice: 15,
+      totalPrice: 30,
+    },
+  ],
+  id: 'IIDSAT',
+  estimatedDelivery: '2027-04-25T06:42:22',
+  // estimatedDelivery: "2027-04-25T10:00:00",
 
-//   orderPrice: 147,
-//   priorityPrice: 29,
-// };
+  orderPrice: 147,
+  priorityPrice: 29,
+};
 
 export default function Order() {
-  const order = useLoaderData();
-  console.log(order);
+  // const order = useLoaderData();
+  // console.log(order);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -119,29 +120,50 @@ export default function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="space-y-9 px-4 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order # {id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="flex flex-wrap gap-2">
+          {priority && (
+            <span className="rounded-full bg-red-500 px-3 py-1 font-semibold uppercase tracking-wide text-red-50">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 font-semibold uppercase tracking-wide text-green-50">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+            : 'Order should have arrived'}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <ul className="divide-y divide-stone-200 border-y">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
+      <div className="space-y-2 bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium text-stone-600">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="font-bold text-stone-600">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
